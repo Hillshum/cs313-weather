988f36e70e667f905133e4ab9275733f9bee300f
Hilton Shumway <hillshum@gmail.com>
2017-06-21 09:54:41 -0600

    Pull Yahoo weather data and use Node v8

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+
+const getWeather = require('./lib/getWeather');
+const app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +15,8 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get("/weather/:location", getWeather)
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
